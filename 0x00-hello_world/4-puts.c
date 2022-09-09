ef4f9ea9883f71ff0769aabcc0e9bd67ec0639bb
josephkathumba <jkathumba@gmail.com>
2022-09-09 15:50:12 -0700

    code that executes puts code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+ *main that uses function puts to display output
+ *
+ *always returns 0
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+
+}
