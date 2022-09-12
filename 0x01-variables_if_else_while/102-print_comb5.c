936a4116df58f639ef1d022bcedf540a72bafded
josephkathumba <jkathumba@gmail.com>
2022-09-12 16:17:21 -0700

    prints 00-99

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers between 00 to 99.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, e, g, h, x, y;
+i = e = g = h = 48;
+while (h < 58)
+{ g = 48;
+while (g < 58)
+{ e = 48;
+while (e < 58)
+{ i = 48;
+while (i < 58)
+{ x = (h * 10) + g;
+y = (e * 10) + i;
+if (x < y)
+{ putchar(h);
+putchar(g);
+putchar(' ');
+putchar(e);
+putchar(i);
+if (h == 57 && g == 56 && e == 57 && i == 57)
+break;
+putchar(',');
+putchar(' ');
+} i++;
+} e++;
+} g++;
+} h++;
+} putchar('\n');
+return (0);
+}
