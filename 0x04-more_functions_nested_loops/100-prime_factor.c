669805afb96ad7d3caaa9d6bfa4e2b785e0ec2e5
josephkathumba <jkathumba@gmail.com>
2022-09-16 06:16:17 -0700

    prime numbers"

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,12 +8,12 @@ int main(void)
 {
 long int n, l;
 n = 612852475143;
-for (l = 2;l <= n; l++)
+for (l = 2; l <= n; l++)
 {
 if (n % l == 0)
 {
 n /= l;
-l = l--;
+l = l - 1;
 }
 }
 printf("%ld\n", l);
