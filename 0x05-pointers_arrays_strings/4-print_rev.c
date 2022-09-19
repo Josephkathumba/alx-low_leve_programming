ed697e2cd34e732b9ccdfd4ce1ed75bb6b3a3cc6
josephkathumba <jkathumba@gmail.com>
2022-09-19 04:16:41 -0700

    reverse prib

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse.
+ * @s: input string.
+ * Return: no return.
+ */
+
+void print_rev(char *s)
+{
+int cnt = 0;
+while (cnt >= 0)
+{
+if (s[cnt] == '\0')
+break;
+cnt++;
+}
+for (cnt--; cnt >= 0; cnt--)
+_putchar(s[cnt]);
+_putchar('\n');
+}
